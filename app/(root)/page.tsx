72f003c462d8689248e25d7c0d2c4ff442ba9b0a
ipunganwar <ipung.anwar1@gmail.com>
2025-03-23 01:17:30 +0700

    initial project

---
+++ app/(root)/page.tsx
@@ -0,0 +1,38 @@
+import Headerbox from "@/components/Headerbox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+import React from "react";
+
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <Headerbox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.name || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.45 }, { currentBalance: 0.45 }]}
+      />
+    </section>
+  );
+};
+
+export default Home;
---
+++ components/Sidebar.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+import Footer from "./Footer";
+
+const Sidebar = ({ user }: SiderbarProps) => {
+  const pathname = usePathname();
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+          <Image
+            src="/icons/logo.svg"
+            alt="Logo"
+            width={34}
+            height={34}
+            className="size-[24] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">Horizon</h1>
+        </Link>
+        {sidebarLinks?.map((item) => {
+          const isActive =
+            pathname === item?.route || pathname?.startsWith(`${item?.route}/`);
+          return (
+            <Link
+              href={item?.route}
+              key={item?.label}
+              className={cn("sidebar-link", { "bg-bankGradient": isActive })}
+            >
+              <div className="relative size-6">
+                <Image
+                  alt={item?.label}
+                  src={item?.imgURL}
+                  fill
+                  className={cn({ "brightness-[3] invert-0": isActive })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item?.label}
+              </p>
+            </Link>
+          );
+        })}
+        User
+      </nav>
+      <Footer user={user} />
+    </section>
+  );
+};
+
+export default Sidebar;
